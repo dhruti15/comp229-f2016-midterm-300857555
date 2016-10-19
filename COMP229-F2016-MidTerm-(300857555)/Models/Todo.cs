4fc81628a79b66446ba25799ded301b2af1da2b7
Dhrutiben Patel <dpate483@my.centennialcollege.ca>
2016-10-19 19:08:20 +0100

    Created project

--- COMP229-F2016-MidTerm-(300857555)/Models/Todo.cs
+++ COMP229-F2016-MidTerm-(300857555)/Models/Todo.cs
@@ -11,9 +11,8 @@ namespace COMP229_F2016_MidTerm__300857555_.Models
     {
         public int TodoID { get; set; }
 
-        [Required]
         [StringLength(50)]
-        public string TodoName { get; set; }
+        public string TodoDescription { get; set; }
 
         public string TodoNotes { get; set; }
 
